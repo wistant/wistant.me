3e2c95c861e24ba04141805dc3b4b3be718084e5
Wistant Kode <wistantkode@protonmail.com>
2026-02-20 02:56:07 +0100

    fix(ui): resoive TypeScript errors in 3d-card and safari components

--- src/components/aceternity/3d-card.tsx
+++ src/components/aceternity/3d-card.tsx
@@ -134,14 +134,15 @@ export const CardItem = ({
     }
   };
 
+  const Component = Tag as any;
   return (
-    <Tag
+    <Component
       ref={ref}
       className={cn("w-fit transition duration-200 ease-linear", className)}
       {...rest}
     >
       {children}
-    </Tag>
+    </Component>
   );
 };
 
