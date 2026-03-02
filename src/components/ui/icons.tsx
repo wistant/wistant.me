4dbf2a3c69111f5043ddf860a723e500246c8560
Wistant Kode <wistantkode@protonmail.com>
2026-03-02 19:08:25 +0100

    feat(repo): add new icons and update personal data structure

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -1,10 +1,12 @@
-import { GlobeIcon, MailIcon } from "lucide-react";
+import { GlobeIcon, MailIcon, TrophyIcon, UserIcon } from "lucide-react";
 
 export type IconProps = React.HTMLAttributes<SVGElement>;
 
 export const Icons = {
   globe: (props: IconProps) => <GlobeIcon {...props} />,
   email: (props: IconProps) => <MailIcon {...props} />,
+  user: (props: IconProps) => <UserIcon {...props} />,
+  trophy: (props: IconProps) => <TrophyIcon {...props} />,
   linkedin: (props: IconProps) => (
     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
       <title>LinkedIn</title>
